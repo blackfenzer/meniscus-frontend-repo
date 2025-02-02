77efc3c800fabb372f74039319130351fe476465
blackfenzer <6431346921@student.chula.ac.th>
2025-02-02 20:44:45 +0700

    sign in with credential

--- app/(dashboard)/user.tsx
+++ app/(dashboard)/user.tsx
@@ -51,7 +51,7 @@ export async function User() {
           </DropdownMenuItem>
         ) : (
           <DropdownMenuItem>
-            <Link href="/login">Sign In</Link>
+            <Link href="/api/auth/signin">Sign In</Link>
           </DropdownMenuItem>
         )}
       </DropdownMenuContent>
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,42 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import CredentialsProvider from "next-auth/providers/credentials"
+import { use } from 'react';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub]
+  providers: [GitHub,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        try {
+          const response = await fetch("http://localhost:8000/token", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              username: credentials?.username,
+              password: credentials?.password,
+            }),
+          });
+
+          if (!response.ok) {
+            throw new Error("Invalid credentials");
+          }
+
+          const data = await response.json();
+          if (response.ok && data) {
+            return data
+          }
+          return { id: credentials?.username, token: data.access_token };
+        } catch (error) {
+          console.error("Login failed:", error);
+          return null;
+        }
+      },
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET
 });

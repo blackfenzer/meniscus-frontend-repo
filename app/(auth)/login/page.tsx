26667bbe70d5aadd650c32e0971dc240483afb79
Songblabla <thanavut32769@gmail.com>
2025-03-06 20:10:11 +0700

    add validate password in backend

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,21 +1,21 @@
-"use client";
+'use client';
 
-import { FormEvent, ChangeEvent, useState } from "react";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import axios from "axios";
-import Cookies from "js-cookie";
-import Footer from "@/components/footer/page";
-import { toast } from "react-hot-toast";
-import { useUser } from "context/UserContext";
+import { FormEvent, ChangeEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import Footer from '@/components/footer/page';
+import { toast } from 'react-hot-toast';
+import { useUser } from 'context/UserContext';
 
 export default function LoginPage() {
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
   const router = useRouter();
-  const [formData, setFormData] = useState({ username: "", password: "" });
+  const [formData, setFormData] = useState({ username: '', password: '' });
   const { fetchUser } = useUser();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -24,7 +24,7 @@ export default function LoginPage() {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    setError("");
+    setError('');
 
     try {
       const response = await axios.post(
@@ -32,18 +32,21 @@ export default function LoginPage() {
       );
 
       if (response.status === 200) {
-        Cookies.set('session_token', response.data.access_token, { expires: 1 }); // 1 day expiration for example
+        toast.success('Login successful');
+        Cookies.set('session_token', response.data.access_token, {
+          expires: 1
+        }); // 1 day expiration for example
         Cookies.set('csrf_token', response.data.csrf_token, { expires: 1 });
-        toast.success("Login successful");
+
         await fetchUser();
-        router.push("/");
-      } 
+        router.push('/');
+      }
       // else {
       //   throw new Error("Login failed");
       // }
     } catch (error) {
       // toast.error("Invalid username or password");
-      setError("Invalid username or password");
+      setError('Invalid username or password');
     }
   };
 
@@ -53,13 +56,32 @@ export default function LoginPage() {
         <CardContent>
           <h1 className="text-2xl font-bold mb-4">Login</h1>
           <form onSubmit={handleSubmit} className="space-y-4">
-            <Input required name="username" placeholder="Username" onChange={handleChange} />
-            <Input required name="password" type="password" placeholder="Password" onChange={handleChange} />
+            <Input
+              required
+              name="username"
+              placeholder="Username"
+              onChange={handleChange}
+            />
+            <Input
+              required
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
             {error && <p className="text-red-500 text-sm">{error}</p>}
-            <Button type="submit" className="w-full">Sign In</Button>
+            <Button type="submit" className="w-full">
+              Sign In
+            </Button>
           </form>
           <p className="text-center text-gray-500 mt-4">
-            Don't have an account? <Link href="/register" className="text-gray-800 dark:text-gray-200 hover:underline font-semibold">Sign Up</Link>
+            Don't have an account?{' '}
+            <Link
+              href="/register"
+              className="text-gray-800 dark:text-gray-200 hover:underline font-semibold"
+            >
+              Sign Up
+            </Link>
           </p>
         </CardContent>
       </Card>

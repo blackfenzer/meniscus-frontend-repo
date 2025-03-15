82438896e28169f7c1b584d0f0e9c24b982b08eb
blackfenzer <6431346921@student.chula.ac.th>
2025-03-15 18:56:26 +0700

    auth user crud

--- lib/axios.ts
+++ lib/axios.ts
@@ -4,7 +4,8 @@ const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, // Ensure this environment variable is set
   headers: {
     'Content-Type': 'application/json'
-  }
+  },
+  withCredentials: true,
 });
 
 export default apiClient;
